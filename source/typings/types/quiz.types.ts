f6b71c837075e739a271022ae66606b486ff8509
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-08-12 20:54:49 +0300

    type fix

--- source/typings/types/quiz.types.ts
+++ source/typings/types/quiz.types.ts
@@ -1,3 +1,7 @@
+import {IQuestion} from "../interfaces/quiz.interfaces";
+
 export type GameTopicsType<T> = {
     [Key in keyof typeof T]: T[Key]
 };
+
+export type QuestionCollection = Record<string, IQuestion[]>;
